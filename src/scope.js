c1cd086e152856725c2eb8f05806280d3229493c
Mudit Ameta <zeusdeux@gmail.com>
2015-05-08 01:01:58 -0400

    Add debug logging

--- src/scope.js
+++ src/scope.js
@@ -1,9 +1,14 @@
+var d = require('./util').log;
+
+
 exports.createScope = function(obj, parentScope) {
   return (obj.__parent__ = parentScope) && obj;
 };
 
 exports.findInScope = function findInScope(scope, identifier) {
-  if ({} === scope) return void 0;
+  d(scope);
+  d(identifier);
+  if (0 === Object.keys(scope).length) throw new ReferenceError(identifier + ' is not defined');
   if (scope.hasOwnProperty(identifier)) return scope[identifier];
   return findInScope(scope.__parent__, identifier);
 };
