0fb58c1f035a74de51af111feee0fe8ca83ed69d
Mudit Ameta <zeusdeux@gmail.com>
2015-05-14 03:42:42 -0400

    Change var to const and fix some logging

--- src/scope.js
+++ src/scope.js
@@ -1,13 +1,17 @@
-var d = require('./util').log;
+const d = require('./util').log;
 
 
 exports.createScope = function(obj, parentScope) {
+  obj = obj || {};
   return (obj.__parent__ = parentScope) && obj;
 };
 
 exports.findInScope = function findInScope(scope, identifier) {
+  d('findInScope:');
   d(scope);
   d(identifier);
+  d('/findInScope');
+
   if (0 === Object.keys(scope).length) throw new ReferenceError(identifier + ' is not defined');
   if (scope.hasOwnProperty(identifier)) return scope[identifier];
   return findInScope(scope.__parent__, identifier);
