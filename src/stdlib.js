97338d6bb1225c1fa72ccef7d16b103522c8bc71
Mudit Ameta <zeusdeux@gmail.com>
2015-05-19 03:35:42 -0500

    Add math operations to stdlib

--- src/stdlib.js
+++ src/stdlib.js
@@ -1,3 +1,12 @@
+'use strict';
+
+let math = Object.getOwnPropertyNames(Math).reduce((p, c) => {
+  p[c] = Math[c];
+  return p;
+}, {});
+
+
 module.exports = {
-  print: console.log.bind(console)
+  print: console.log.bind(console),
+  Math: math
 };
