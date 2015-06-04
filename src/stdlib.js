48c9e4eeb8453187d1382ae52bf0dc6017ba7692
Mudit Ameta <zeusdeux@gmail.com>
2015-06-04 05:44:23 -0400

    Add push and pop

--- src/stdlib.js
+++ src/stdlib.js
@@ -8,5 +8,21 @@ let math = Object.getOwnPropertyNames(Math).reduce((p, c) => {
 
 module.exports = {
   print: console.log.bind(console),
-  Math: math
+  Math: math,
+  push: function(list, val, key) {
+    if (Array.isArray(list)) {
+      list.push(val);
+      return list;
+    }
+    if ('object' === typeof list && null !== list) {
+      if (!key) throw new SyntaxError('push needs a key as the last arg when used with a hashmap');
+      list[key] = val;
+      return list;
+    }
+    throw new SyntaxError('push can only be used with arrays and hashmaps');
+  },
+  pop: function(list) {
+    if (Array.isArray(list)) return list.pop();
+    throw new SyntaxError('pop can only be used with arrays');
+  }
 };
