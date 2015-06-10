c8638ecc9142be7e0469c98c6e019ab97301cc90
Mudit Ameta <zeusdeux@gmail.com>
2015-06-10 11:25:15 -0400

    Make stdlib array/list functions non-side-effecty

--- src/core/lib/stdlib.js
+++ src/core/lib/stdlib.js
@@ -1,25 +1,33 @@
 'use strict';
 
+const clone = require('clone');
+
+
 module.exports = {
   print: console.log.bind(console),
   push: function(list, val, key) {
     if (Array.isArray(list)) {
-      list.push(val);
-      return list;
+      let tempList = list.slice(0);
+
+      tempList.push(val);
+      return tempList;
     }
     if ('object' === typeof list && null !== list) {
+      let tempMap = clone(list);
+
       if (!key) throw new SyntaxError('push needs a key as the last arg when used with a hashmap');
-      list[key] = val;
-      return list;
+      tempMap[key] = val;
+      return tempMap;
     }
     throw new SyntaxError('push can only be used with arrays and hashmaps');
   },
   pop: function(list) {
-    if (Array.isArray(list)) return list.pop();
+    let tempList = list.slice(0);
+
+    if (Array.isArray(list)) return tempList.pop();
     throw new SyntaxError('pop can only be used with arrays');
   },
   map: function(fn, list) {
-
     if('function' === typeof fn) {
       if (Array.isArray(list)) return list.map(fn);
       throw new SyntaxError('map needs an array as its second parameter');
